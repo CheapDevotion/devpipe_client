1920b5ecbcfae7fb332ba8e50f95d2647422cb79
Jacob Williams <cheapdevotion@gmail.com>
2012-01-25 15:35:21 -0600

    Added support for cased and underscored project names

--- test_server/server.js
+++ test_server/server.js
@@ -1,5 +1,11 @@
 /*jslint undef: true, unparam: true, sloppy: true, maxerr: 50, indent: 4 */
+String.prototype.capitalize = function(){
+   return this.replace( /(^|\s)([a-z])/g , function(m,p1,p2){ return p1+p2.toUpperCase(); } );
+  };
 
+String.prototype.addSpaces = function(){
+   return this.replace(/([a-z])([A-Z])/g, '$1 $2');
+  };
 
 var io = require('socket.io').listen(1001);
 io.configure(function () {
@@ -14,18 +20,17 @@ function broadcastMessage(project, message) {
 	io.sockets.emit('pipe', {project: project, message: JSON.parse(message)});
 }
 
-io.sockets.on('connection', function (socket) {
-	console.log("got connection: " + socket);
-});
-
-
 //Create the simple webserver
 var app = express.createServer();
 app.use(express.bodyParser());
 
 app.post('/project/:project', function (req, res) {
-	console.log(req);
-	broadcastMessage(req.params.project, req.body.message);
+	var project = req.params.project.replace(/_/g, " ");
+	project = project.capitalize();
+	project = project.addSpaces();
+	for (i in req.body){
+			broadcastMessage(project, req.body[i]);
+	}
 	res.send('status: ok');
 });
 
